d771c8f5268fed1a64a72b6e3af36e5125482cce
Joan Solignac Lovera <joanpsolignac@gmail.com>
2026-08-27 15:24:09 -0500

    project: proyecto base

---
+++ src/main/java/com/ggilos/creatorai/CreatorAiApplication.java
@@ -0,0 +1,13 @@
+package com.ggilos.creatorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatorAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggilos/creatorai/CreatorAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggilos.creatorai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
